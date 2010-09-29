273ce39ebeb3a8091d07a7cf2e368dcd456836b9
Will Holtkamp <holtkampw@gmail.com>
2010-09-29 14:16:26 -0500

    Instancing?

--- InstancedModelPipeline/InstancedModelProcessor.cs
+++ InstancedModelPipeline/InstancedModelProcessor.cs
@@ -8,9 +8,6 @@
 #endregion
 
 #region Using Statements
-using System.Collections.Generic;
-using Microsoft.Xna.Framework;
-using Microsoft.Xna.Framework.Graphics;
 using Microsoft.Xna.Framework.Content.Pipeline;
 using Microsoft.Xna.Framework.Content.Pipeline.Graphics;
 using Microsoft.Xna.Framework.Content.Pipeline.Processors;
@@ -19,180 +16,49 @@ using Microsoft.Xna.Framework.Content.Pipeline.Processors;
 namespace InstancedModelPipeline
 {
     /// <summary>
-    /// Content Pipeline processor converts incoming
-    /// graphics data into our custom instanced model format.
+    /// Content Pipeline processor applies the InstancedModel.fx shader
+    /// onto models, so they can be drawn using hardware instancing.
     /// </summary>
     [ContentProcessor(DisplayName = "Instanced Model")]
-    public class InstancedModelProcessor : ContentProcessor<NodeContent,
-                                                            InstancedModelContent>
+    public class InstancedModelProcessor : ModelProcessor
     {
-        #region Fields
+        ContentIdentity rootIdentity;
 
-        NodeContent rootNode;
-        ContentProcessorContext context;
-        InstancedModelContent outputModel;
-
-        // A single material may be reused on more than one piece of geometry.
-        // This dictionary keeps track of materials we have already converted,
-        // to make sure we only bother processing each of them once.
-        Dictionary<MaterialContent, MaterialContent> processedMaterials =
-                            new Dictionary<MaterialContent, MaterialContent>();
-
-        #endregion
 
         /// <summary>
-        /// Converts incoming graphics data into our instanced model format.
+        /// Override the Process method to store the ContentIdentity of the model root node.
         /// </summary>
-        public override InstancedModelContent Process(NodeContent input,
-                                                      ContentProcessorContext context)
+        public override ModelContent Process(NodeContent input, ContentProcessorContext context)
         {
-            this.rootNode = input;
-            this.context = context;
+            rootIdentity = input.Identity;
 
-            outputModel = new InstancedModelContent();
-
-            ProcessNode(input);
-
-            return outputModel;
+            return base.Process(input, context);
         }
 
 
         /// <summary>
-        /// Recursively processes a node from the input data tree.
+        /// Override the ConvertMaterial method to apply our custom InstancedModel.fx shader.
         /// </summary>
-        void ProcessNode(NodeContent node)
+        protected override MaterialContent ConvertMaterial(MaterialContent material,
+                                                           ContentProcessorContext context)
         {
-            // Bake node transforms into the geometry, so we won't have
-            // to bother dealing with these in the runtime drawing code.
-            MeshHelper.TransformScene(node, node.Transform);
+            // Create a new material.
+            EffectMaterialContent newMaterial = new EffectMaterialContent();
 
-            node.Transform = Matrix.Identity;
+            // Tell it to use our custom InstancedModel.fx shader.
+            newMaterial.Effect = new ExternalReference<EffectContent>("InstancedModel.fx",
+                                                                      rootIdentity);
 
-            // Is this node in fact a mesh?
-            MeshContent mesh = node as MeshContent;
+            // Copy the texture setting across from the original material.
+            BasicMaterialContent basicMaterial = material as BasicMaterialContent;
 
-            if (mesh != null)
+            if ((basicMaterial != null) && (basicMaterial.Texture != null))
             {
-                // Reorder vertex and index data so triangles will render in
-                // an order that makes efficient use of the GPU vertex cache.
-                MeshHelper.OptimizeForCache(mesh);
-
-                // Process all the geometry in the mesh.
-                foreach (GeometryContent geometry in mesh.Geometry)
-                {
-                    ProcessGeometry(geometry);
-                }
+                newMaterial.Textures.Add("Texture", basicMaterial.Texture);
             }
 
-            // Recurse over any child nodes.
-            foreach (NodeContent child in node.Children)
-            {
-                ProcessNode(child);
-            }
-        }
-
-
-        /// <summary>
-        /// Converts a single piece of input geometry into our instanced format.
-        /// </summary>
-        void ProcessGeometry(GeometryContent geometry)
-        {
-            int indexCount = geometry.Indices.Count;
-            int vertexCount = geometry.Vertices.VertexCount;
-
-            // Validate that the number of vertices is suitable for instancing.
-            if (vertexCount > ushort.MaxValue)
-            {
-                throw new InvalidContentException(
-                    string.Format("Geometry contains {0} vertices: " +
-                                  "this is too many to be instanced.", vertexCount));
-            }
-
-            if (vertexCount > ushort.MaxValue / 8)
-            {
-                context.Logger.LogWarning(null, rootNode.Identity,
-                                          "Geometry contains {0} vertices: " +
-                                          "this will only allow it to be instanced " +
-                                          "{1} times per batch. A model with fewer " +
-                                          "vertices would be more efficient.",
-                                          vertexCount, ushort.MaxValue / vertexCount);
-            }
-
-            // Validate that the vertex channels we are going to use to pass
-            // through our instancing data aren't already in use.
-            VertexChannelCollection vertexChannels = geometry.Vertices.Channels;
-
-            for (int i = 1; i <= 4; i++)
-            {
-                if (vertexChannels.Contains(VertexChannelNames.TextureCoordinate(i)))
-                {
-                    throw new InvalidContentException(
-                        string.Format("Model already contains data for texture " +
-                                      "coordinate channel {0}, but instancing " +
-                                      "requires this channel for its own use.", i));
-                }
-            }
-
-            // Flatten the flexible input vertex channel data into
-            // a simple GPU style vertex buffer byte array.
-            VertexBufferContent vertexBufferContent;
-            VertexElement[] vertexElements;
-
-
-            vertexBufferContent = geometry.Vertices.CreateVertexBuffer();
-            int vertexStride = (int)vertexBufferContent.VertexDeclaration.VertexStride;
-
-            //vertexElements = vertexBufferContent.VertexData
-            /*geometry.Vertices.CreateVertexBuffer(out vertexBufferContent,
-                                                 out vertexElements,
-                                                 context.TargetPlatform);*/
-
-            //int vertexStride = VertexDeclaration.GetVertexStrideSize(vertexElements, 0);
-
-            // Convert the input material.
-            MaterialContent material = ProcessMaterial(geometry.Material);
-
-            // Add the new piece of geometry to our output model.
-            outputModel.AddModelPart(indexCount, vertexCount, vertexStride,
-                /*vertexElements,*/ vertexBufferContent,
-                                     geometry.Indices, material);
-        }
-
-
-        /// <summary>
-        /// Converts an input material to use our custom InstancedModel effect.
-        /// </summary>
-        MaterialContent ProcessMaterial(MaterialContent material)
-        {
-            // Have we already processed this material?
-            if (!processedMaterials.ContainsKey(material))
-            {
-                // If not, process it now.
-                EffectMaterialContent instancedMaterial = new EffectMaterialContent();
-
-                // Set the material to use our custom instancing effect.
-                instancedMaterial.Effect = new ExternalReference<EffectContent>(
-                                                "InstancedModel.fx", rootNode.Identity);
-
-                // Copy across the texture setting from the input material.
-                if (!material.Textures.ContainsKey("Texture"))
-                {
-                    throw new InvalidContentException(
-                        "Material has no texture, but the InstancedModel " +
-                        "effect does not support untextured materials.");
-                }
-
-                instancedMaterial.Textures.Add("Texture", material.Textures["Texture"]);
-
-                // Chain to the built-in MaterialProcessor, which will
-                // build the effect and texture referenced by this material.
-                processedMaterials[material] =
-                    context.Convert<MaterialContent,
-                                    MaterialContent>(instancedMaterial,
-                                                     "MaterialProcessor");
-            }
-
-            return processedMaterials[material];
+            // Chain to the base ModelProcessor, so it can build our new material.
+            return base.ConvertMaterial(newMaterial, context);
         }
     }
 }
--- UHSampleGame/Player/Player.cs
+++ UHSampleGame/Player/Player.cs
@@ -14,24 +14,41 @@ using UHSampleGame.TileSystem;
 using UHSampleGame.InputManagement;
 using UHSampleGame.ScreenManagement;
 using UHSampleGame.CameraManagement;
-using UHSampleGame.InstancedModels;
 
 namespace UHSampleGame.Player
 {
+    public enum InstancingTechnique
+    {
+        HardwareInstancing,
+        NoInstancing,
+        NoInstancingOrStateBatching
+    }
+
     public abstract class Player
     {
+        static VertexDeclaration instanceVertexDeclaration = new VertexDeclaration
+        (
+            new VertexElement(0, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 0),
+            new VertexElement(16, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 1),
+            new VertexElement(32, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 2),
+            new VertexElement(48, VertexElementFormat.Vector4, VertexElementUsage.BlendWeight, 3)
+        );
+
         protected Base playerBase;
 
         protected List<Tower> towers;
         protected Dictionary<UnitType, List<Unit>> units;
         protected Dictionary<UnitType, int> previousCount;
-        protected Dictionary<UnitType, InstancedModel> instancedModels;
+        protected Dictionary<UnitType, Model> instancedModels;
+        protected Dictionary<UnitType, Matrix[]> instancedModelBones;
         protected int money;
         protected int playerNum;
         protected int teamNum;
 
         protected SkinnedEffect genericEffect;
         protected CameraManager cameraManager;
+
+        InstancingTechnique instancingTechnique = InstancingTechnique.HardwareInstancing;
 
         //Transforms
         Dictionary<UnitType, List<Matrix>> unitTransforms;
@@ -78,27 +95,24 @@ namespace UHSampleGame.Player
             this.units = new Dictionary<UnitType, List<Unit>>();
             previousCount = new Dictionary<UnitType, int>();
             unitTransforms = new Dictionary<UnitType, List<Matrix>>();
-            instancedModels = new Dictionary<UnitType, InstancedModel>();
+            instancedModels = new Dictionary<UnitType, Model>();
             foreach(UnitType key in Enum.GetValues(typeof(UnitType)))
             {
                 units[key] = new List<Unit>();
                 previousCount[key] = 0;
                 unitTransforms[key] = new List<Matrix>();
-                instancedModels[key] = new InstancedModel(ScreenManager.Game.GraphicsDevice);
-                switch(key)
+
+                switch (key)
                 {
                     case UnitType.TestUnit:
-                        instancedModels[key].Setup(TestUnit.Model);
+                        instancedModels[key] = TestUnit.Model;
+                        instancedModels[key].CopyAbsoluteBoneTransformsTo(instancedModelBones[key]);
                         break;
                 }
             }
             this.money = 0;
             this.cameraManager = (CameraManager)ScreenManager.Game.Services.GetService(typeof(CameraManager));
-
-            this.genericEffect = new SkinnedEffect(ScreenManager.Game.GraphicsDevice);
-            genericEffect.View = cameraManager.ViewMatrix;
-            genericEffect.Projection = cameraManager.ProjectionMatrix;
-            
+            this.genericEffect = new SkinnedEffect(ScreenManager.Game.GraphicsDevice);         
         }
 
         public void SetTargetBase(Base target)
@@ -180,16 +194,190 @@ namespace UHSampleGame.Player
 
         private void DrawUnits(GameTime gameTime)
         {
-            genericEffect.World = cameraManager.ViewMatrix;
-
+            // Draw all the instances, using the currently selected rendering technique.
             foreach (UnitType key in Enum.GetValues(typeof(UnitType)))
             {
-                if (unitTransforms[key].Count > 0)
+                Matrix[] transforms = new Matrix[units[key].Count];
+                for(int i =0 ; i < units[key].Count; i++) {
+                    transforms[i] = units[key][i].Transforms;
+                }
+
+                switch (instancingTechnique)
                 {
-                    Matrix[] tempTransforms = unitTransforms[key].ToArray();
-                    instancedModels[key].Draw(ref tempTransforms, ref genericEffect);
+                    case InstancingTechnique.HardwareInstancing:
+                        DrawModelHardwareInstancing(instancedModels[key], instancedModelBones[key],
+                                                    transforms, cameraManager.ViewMatrix, cameraManager.ProjectionMatrix);
+                        break;
+
+                    case InstancingTechnique.NoInstancing:
+                        DrawModelNoInstancing(instancedModels[key], instancedModelBones[key],
+                                              transforms, cameraManager.ViewMatrix, cameraManager.ProjectionMatrix);
+                        break;
+
+                    case InstancingTechnique.NoInstancingOrStateBatching:
+                        DrawModelNoInstancingOrStateBatching(instancedModels[key], instancedModelBones[key],
+                                              transforms, cameraManager.ViewMatrix, cameraManager.ProjectionMatrix);
+                        break;
                 }
             }
         }
+
+        #region Instancing Helpers
+        /// <summary>
+        /// Efficiently draws several copies of a piece of geometry using hardware instancing.
+        /// </summary>
+        void DrawModelHardwareInstancing(Model model, Matrix[] modelBones,
+                                         Matrix[] instances, Matrix view, Matrix projection)
+        {
+            DynamicVertexBuffer instanceVertexBuffer = null;
+            if (instances.Length == 0)
+                return;
+
+            // If we have more instances than room in our vertex buffer, grow it to the neccessary size.
+            if ((instanceVertexBuffer == null) ||
+                (instances.Length > instanceVertexBuffer.VertexCount))
+            {
+                if (instanceVertexBuffer != null)
+                    instanceVertexBuffer.Dispose();
+
+                instanceVertexBuffer = new DynamicVertexBuffer(ScreenManager.Game.GraphicsDevice, instanceVertexDeclaration,
+                                                               instances.Length, BufferUsage.WriteOnly);
+            }
+
+            // Transfer the latest instance transform matrices into the instanceVertexBuffer.
+            instanceVertexBuffer.SetData(instances, 0, instances.Length, SetDataOptions.Discard);
+
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                {
+                    // Tell the GPU to read from both the model vertex buffer plus our instanceVertexBuffer.
+                    ScreenManager.Game.GraphicsDevice.SetVertexBuffers(
+                        new VertexBufferBinding(meshPart.VertexBuffer, meshPart.VertexOffset, 0),
+                        new VertexBufferBinding(instanceVertexBuffer, 0, 1)
+                    );
+
+                    ScreenManager.Game.GraphicsDevice.Indices = meshPart.IndexBuffer;
+
+                    // Set up the instance rendering effect.
+                    Effect effect = meshPart.Effect;
+
+                    effect.CurrentTechnique = effect.Techniques["HardwareInstancing"];
+
+                    effect.Parameters["World"].SetValue(modelBones[mesh.ParentBone.Index]);
+                    effect.Parameters["View"].SetValue(view);
+                    effect.Parameters["Projection"].SetValue(projection);
+
+                    // Draw all the instance copies in a single call.
+                    foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                    {
+                        pass.Apply();
+
+                        ScreenManager.Game.GraphicsDevice.DrawInstancedPrimitives(PrimitiveType.TriangleList, 0, 0,
+                                                               meshPart.NumVertices, meshPart.StartIndex,
+                                                               meshPart.PrimitiveCount, instances.Length);
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// Draws several copies of a piece of geometry without using any
+        /// special GPU instancing techniques at all. This just does a
+        /// regular loop and issues several draw calls one after another.
+        /// </summary>
+        void DrawModelNoInstancing(Model model, Matrix[] modelBones,
+                                   Matrix[] instances, Matrix view, Matrix projection)
+        {
+            foreach (ModelMesh mesh in model.Meshes)
+            {
+                foreach (ModelMeshPart meshPart in mesh.MeshParts)
+                {
+                    ScreenManager.Game.GraphicsDevice.SetVertexBuffer(meshPart.VertexBuffer, meshPart.VertexOffset);
+                    ScreenManager.Game.GraphicsDevice.Indices = meshPart.IndexBuffer;
+
+                    // Set up the rendering effect.
+                    Effect effect = meshPart.Effect;
+
+                    effect.CurrentTechnique = effect.Techniques["NoInstancing"];
+
+                    effect.Parameters["View"].SetValue(view);
+                    effect.Parameters["Projection"].SetValue(projection);
+
+                    EffectParameter transformParameter = effect.Parameters["World"];
+
+                    // Draw a single instance copy each time around this loop.
+                    for (int i = 0; i < instances.Length; i++)
+                    {
+                        transformParameter.SetValue(modelBones[mesh.ParentBone.Index] * instances[i]);
+
+                        foreach (EffectPass pass in effect.CurrentTechnique.Passes)
+                        {
+                            pass.Apply();
+
+                            ScreenManager.Game.GraphicsDevice.DrawIndexedPrimitives(PrimitiveType.TriangleList, 0, 0,
+                                                                 meshPart.NumVertices, meshPart.StartIndex,
+                                                                 meshPart.PrimitiveCount);
+                        }
+                    }
+                }
+            }
+        }
+
+
+        /// <summary>
+        /// This technique is NOT a good idea! It is only included in the sample
+        /// for comparison purposes, so you can compare its performance with the
+        /// other more sensible approaches. This uses the exact same shader code
+        /// as the preceding NoInstancing technique, but with a key difference.
+        /// Where the NoInstancing technique worked like this:
+        /// 
+        ///     SetRenderStates()
+        ///     foreach instance
+        ///     {
+        ///         Update effect with per-instance transform matrix
+        ///         DrawIndexedPrimitives()
+        ///     }
+        /// 
+        /// NoInstancingOrStateBatching works like so:
+        /// 
+        ///     foreach instance
+        ///     {
+        ///         Set per-instance transform matrix into the effect
+        ///         SetRenderStates()
+        ///         DrawIndexedPrimitives()
+        ///     }
+        ///      
+        /// As you can see, this is repeatedly setting the same renderstates.
+        /// Not so efficient.
+        /// 
+        /// In other words, the built-in Model.Draw method is pretty inefficient when
+        /// it comes to drawing more than one instance! Even without using any fancy
+        /// shader techniques, you can get a significant speed boost just by rearranging
+        /// your drawing code to work more like the earlier NoInstancing technique.
+        /// </summary>
+        void DrawModelNoInstancingOrStateBatching(Model model, Matrix[] modelBones,
+                                                  Matrix[] instances, Matrix view, Matrix projection)
+        {
+            for (int i = 0; i < instances.Length; i++)
+            {
+                foreach (ModelMesh mesh in model.Meshes)
+                {
+                    foreach (Effect effect in mesh.Effects)
+                    {
+                        effect.CurrentTechnique = effect.Techniques["NoInstancing"];
+
+                        effect.Parameters["World"].SetValue(modelBones[mesh.ParentBone.Index] * instances[i]);
+                        effect.Parameters["View"].SetValue(view);
+                        effect.Parameters["Projection"].SetValue(projection);
+                    }
+
+                    mesh.Draw();
+                }
+            }
+        }
+
+        #endregion
     }
 }
