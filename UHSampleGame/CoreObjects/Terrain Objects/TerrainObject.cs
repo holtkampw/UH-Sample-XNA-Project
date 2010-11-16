8e65253010aba8f3f81b79d3402a22489843a285
Phil <philt5252@gmail.com>
2010-11-15 23:44:08 -0600

    whoo hoo tweaking

--- UHSampleGame/CoreObjects/Terrain Objects/TerrainObject.cs
+++ UHSampleGame/CoreObjects/Terrain Objects/TerrainObject.cs
@@ -5,7 +5,7 @@ using System.Text;
 
 namespace UHSampleGame.CoreObjects.Terrain_Objects
 {
-    public enum TerrainObjectType {Bouy };
+    public enum TerrainObjectType {Bouy};
 
     public class TerrainObject
     {
